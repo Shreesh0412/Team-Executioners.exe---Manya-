40f3584a1b0cbf34224119986ec8d7de029b77ae
Shreesh0412 <shreesh.upa2007@gmail.com>
2026-07-30 19:12:08 +0530

    changes

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,11 +1,17 @@
-from . import auth
-from . import users
+"""
+API router package.
+
+This package contains all API route modules used by the application.
+"""
+
 from . import auth
 from . import documents
+from . import folders
 from . import users
 
 __all__ = [
     "auth",
     "documents",
+    "folders",
     "users",
 ]
